8ac7117ad137267179899120b8be946f1415483c
czp <caozhengping@dxchain.onaliyun.com>
2020-11-08 15:36:28 +0800

    [front] home为0.0.2

--- front/src/page/home/index.jsx
+++ front/src/page/home/index.jsx
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return(
             <div style={{ fontSize: 100, margin: '0 auto', width:320, position:'relative', top: '400px' }}>
-                0.0.1
+                0.0.2
             </div>
         )
     }
